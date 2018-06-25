4868df5c6e59f728bf549790dfabd582c76a633b
davide chicco <davide.chicco@gmail.com>
2018-06-25 15:08:04 -0400

    edited behst-download-data.sh

--- bin/behst-download-data.sh
+++ bin/behst-download-data.sh
@@ -46,62 +46,10 @@ cd $OUTDIR
 wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/appris_data_principal.bed
 wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/gencode.v19.annotation_withproteinids.gtf
 wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/hic_8celltypes.hiccups
-wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_LIVER_enhancers.bed
 
-wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/appris_data_principal_bed.pkl
-wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/gencode.v19.annotation_withproteinids_gtf.pkl
-wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/hic_8celltypes_hiccups.pkl
+if [ "$2" == "--small" ]; then 
 
-if [ "$2" != "--small" ]; then 
-
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/my.genome
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_BLOOD_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_BLOOD_enhancers_SHUFFLED.bed      
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_LIVER_enhancers_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_LUNG_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_LUNG_enhancers_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_PANCREAS_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_PANCREAS_enhancers_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_STOMACH_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_STOMACH_enhancers_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_pressto_BLOOD_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_pressto_LIVER_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_pressto_LUNG_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_pressto_PANCREAS_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_pressto_STOMACH_enhancers.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_EYE_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_FOREBRAIN_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_HEART_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_HINDBRAIN_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_Human_BRAIN_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_Human_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_LIMB_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_MIDBRAIN_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/randomlyShuffledNew_vista_NOSE_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_EYE_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_EYE_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_EYE_TEMP
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_FOREBRAIN_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_FOREBRAIN_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_HEART_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_HEART_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_HEART_TEMP
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_HINDBRAIN_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_HINDBRAIN_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_Human_BRAIN_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_Human_BRAIN_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_Human_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_Human_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_LIMB_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_LIMB_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_LIMB_sorted_EDITED_FOR_GREAT.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_MIDBRAIN_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_MIDBRAIN_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_MIDBRAIN_sorted_EDITED_FOR_GREAT.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_NOSE_SHUFFLED.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_NOSE_sorted.bed
-      wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/vista_NOSE_sorted_FOR_GREAT.bed
-
+  wget https://www.pmgenomics.ca/hoffmanlab/proj/behst/data/pressto_LIVER_enhancers.bed
 fi
 
 # Let's also create the /results/ and /temp/ folder that BEHST needs
